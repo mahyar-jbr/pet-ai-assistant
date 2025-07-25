8defa0eaa70d9832355b81b664527fa1312dc57f
Mahyar Jaberi <jaberi.mahyar@gmail.com>
2025-07-24 21:04:23 -0400

    WIP: prep changes before pulling remote

---
+++ backend/demo/src/main/java/com/example/demo/repository/PetRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Pet;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PetRepository extends MongoRepository<Pet, String> {
+    // You can add custom queries later if needed
+}
